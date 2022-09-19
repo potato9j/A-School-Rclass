6d69ad943a84d66788c040f5e54e2d8d706198e9
potato9j <64534095+potato9j@users.noreply.github.com>
2022-09-19 11:57:39 +0900

    Add files via upload

---
+++ 9-19.R
@@ -0,0 +1,38 @@
+summary(iris)
+View(iris)
+
+
+if(Á¶°Ç½Ä){
+  
+} else{
+  
+}
+
+
+menu <- "Â¥Àå"
+if(menu=="Â¥Àå"){
+  print("Â¥Àå¸é Ãß°¡")
+} else{
+  print("Â«»Í Ãß°¡")
+}
+
+
+menu <- "ººÀ½¹ä"
+if(menu=="Â¥Àå"){
+  print("Â¥Àå¸é Ãß°¡")
+} else if(menu=="Â«»Í"){
+  print("Â«»Í Ãß°¡")
+} else {
+  print("ººÀ½¹ä Ãß°¡")
+}
+
+
+
+score <- 78
+if(score >= 91){
+  print("A+")  
+} else if(score >= 81){  #elseif
+  print("A0")
+} else if(score >= 71{
+  print("B+")
+}
