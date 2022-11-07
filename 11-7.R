35177adedd50d87834204eddfd5066a98c0f4adc
potato9j <64534095+potato9j@users.noreply.github.com>
2022-11-07 12:31:41 +0900

    Add files via upload

--- 11-7.R
+++ 11-7.R
@@ -14,7 +14,7 @@ ifelse(enrollment$성적 == 'A+', 4.5,
                                    ifelse(enrollment$성적 =='Co', 2.0, 0))))))
 
 # dplyr를 통한 변수생성
-enrollment <- enrollment %>%
+enrollment %>%
   mutate(성적점수 = ifelse(성적 == 'A+', 4.5, 
                     ifelse(성적 == 'Ao', 4.0, 
                     ifelse(성적 == 'B+', 3.5,
@@ -22,4 +22,11 @@ enrollment <- enrollment %>%
                     ifelse(성적 == 'C+', 2.5,
                     ifelse(성적 =='Co', 2.0, 0))))))) %>%
   mutate(학점수X성적점수 = 학점수*성적점수) %>%
-  
+  group_by(학번 ) %>%
+  summarise(성적점수합 = sum(학점수X성적점수),
+            학점수합 = sum(학점수)) %>%
+  mutate(GPA = 성적점수합/학점수합) %>%
+  select(학번, GPA) %>% 
+  merge(enrollment, by='학번', all.x=TRUE) %>%
+  select(학번, GPA, 소속학과, 학년) %>%
+  write.csv("C:/data/GPA.csv")
